cb38b56626ab2b7ac71d6aeeb450fb651f9d22d3
sitroq1 <sitroq@gmail.com>
2023-06-09 14:39:29 +0300

    feat: add categoris

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,16 +1,36 @@
+import React, { useState } from "react";
+
 function Categories() {
-    return (
-        <div className="categories">
-            <ul>
-                <li className="active">Все</li>
-                <li>Мясные</li>
-                <li>Вегетарианская</li>
-                <li>Гриль</li>
-                <li>Острые</li>
-                <li>Закрытые</li>
-            </ul>
-        </div>
-    );
+  const categories = [
+    "Все",
+    "Мясные",
+    "Вегетарианская",
+    "Гриль",
+    "Острые",
+    "Закрытые",
+  ];
+  let [indexOfCategories, setIndexOfCategories] = useState(0);
+
+  const chooseСategory = (indx) => {
+    setIndexOfCategories(indx);
+  };
+
+  return (
+    <div className="categories">
+      <ul>
+        {categories.map((val, indx) => {
+          return (
+            <li
+              onClick={() => chooseСategory(indx)}
+              className={indexOfCategories === indx ? "active" : ""}
+            >
+              {val}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default Categories;
