7346f9f267fe5cce22d14660860551e12646beb1
sitroq1 <sitroq@gmail.com>
2023-06-09 19:16:39 +0300

    feat: add interactiv for elements

--- src/App.js
+++ src/App.js
@@ -3,34 +3,31 @@ import Header from "./components/Header";
 import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import PizzaBlock from "./components/PizzaBlock ";
+import pizzaBD from "./assets/pizzas.json";
 
 function App() {
-  return(
+  return (
     <div className="wrapper">
-    <Header />
-  <div className="content">
-    <div className="container">
-      <div className="content__top">
-        <Categories />
-        <Sort />
-        
-      </div>
-      <h2 className="content__title">Все пиццы</h2>
-      <div className="content__items">
-        <PizzaBlock title="Чизбургер-пицца" price={395}/>
-        <PizzaBlock />
-        <PizzaBlock />
-        <PizzaBlock />
-        <PizzaBlock />
-        <PizzaBlock />
-        <PizzaBlock />
-
-        
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+            {console.log(pizzaBD)}
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            {pizzaBD.map((obj) => {
+              return (
+                <PizzaBlock {...obj}/>
+              );
+            })}
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</div>
-  )
+  );
 }
 
 export default App;
