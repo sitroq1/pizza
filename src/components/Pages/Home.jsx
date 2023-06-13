ecee26d3390eb9e4513f7596de9ac4c293191955
sitroq1 <sitroq@gmail.com>
2023-06-13 19:11:35 +0300

    feat: add pagination

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -7,53 +7,62 @@ import Categories from "../Categories";
 import Sort from "../Sort";
 import PizzaBlock from "../PizzaBlock/index";
 import Skeleton from "../PizzaBlock/Skeleton";
+import Pagination from "./Pagination";
 
-export const Home = ({searchValue}) => {
 
-    const [productList, setProductList] = useState([]);
-  const [isLoading, setIsLoading] = useState(true)
+export const Home = ({ searchValue }) => {
+  const [productList, setProductList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   let [categorieId, setCategorieId] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [sort, setSort] = useState({
-    name: 'популярности',
-    sortProperty: 'rating'
+    name: "популярности",
+    sortProperty: "rating",
   });
 
-  
   useEffect(() => {
-    setIsLoading(true)
+    setIsLoading(true);
 
-    const sortBy = sort.sortProperty.replace('-', '');
-    const order = sort.sortProperty.includes('-') ? 'asc': 'desc';
-    const category = categorieId > 0 ? `category=${categorieId}`: '';
-    const search  = searchValue ? `&search=${searchValue}`: '';
+    const sortBy = sort.sortProperty.replace("-", "");
+    const order = sort.sortProperty.includes("-") ? "asc" : "desc";
+    const category = categorieId > 0 ? `category=${categorieId}` : "";
+    const search = searchValue ? `&search=${searchValue}` : "";
+    
 
-    fetch(`https://648850e00e2469c038fd750f.mockapi.io/pizza?${category}&sortBy=${sortBy}&order=${order}${search}`)
-    .then((res) => res.json())
-    .then((res) => {
-      setProductList(res)
-      setIsLoading(false)
-    });
-    window.scrollTo(0, 0)
-  }, [categorieId, sort, searchValue])
+    fetch(
+      `https://648850e00e2469c038fd750f.mockapi.io/pizza?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        setProductList(res);
+        setIsLoading(false);
+      });
+    window.scrollTo(0, 0);
+  }, [categorieId, sort, searchValue, currentPage]);
 
-    return (
-      <div className="container">
-            <div className="content__top">
-                <Categories value={categorieId} onClickCategory={(indx) => setCategorieId(indx)}/>
-                <Sort value={sort} onSelectSort={(i) => setSort(i)}/>
-            </div>
-            <h2 className="content__title">Все пиццы</h2>
-            <div className="content__items">
-                {isLoading
-                    ? [...new Array(6)].map((_, indx) => {
-                          return <Skeleton key={indx} />;
-                      })
-                    : productList.map((obj) => {
-                          return <PizzaBlock key={obj.id} {...obj} />;
-                      })}
-            </div>
-        </div>
-    );
+  return (
+    <div className="container">
+      <div className="content__top">
+        <Categories
+          value={categorieId}
+          onClickCategory={(indx) => setCategorieId(indx)}
+        />
+        <Sort value={sort} onSelectSort={(i) => setSort(i)} />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+      
+        {isLoading
+          ? [...new Array(6)].map((_, indx) => {
+              return <Skeleton key={indx} />;
+            })
+          : productList.map((obj) => {
+              return <PizzaBlock key={obj.id} {...obj} />;
+            })}
+      </div>
+      <Pagination onChangePage={(number) => setCurrentPage(number)}/>
+    </div>
+  );
 };
 
-export default Home
+export default Home;
