66a391c517fd5e9775daee7e9bab2f308f29cadc
sitroq1 <sitroq@gmail.com>
2023-06-15 11:01:41 +0300

    feat: add focus input to element after clearing text

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useRef } from "react";
 
 import styles from "./Search.module.scss";
 import { searchContext } from "../../App";
@@ -8,22 +8,27 @@ import { searchContext } from "../../App";
 export default function Search() {
 
 const {searchValue, setSearchValue} = useContext(searchContext)
-
+const inputRef = useRef();
   return (
     <>
       <div className={styles.root}>
         <input
         value={searchValue}
           className={styles.input}
+          ref={inputRef}
           placeholder="Поиск"
           onChange={(event) => setSearchValue(event.target.value)}
         />
 
         {searchValue && (
             <svg
-            onClick={() => setSearchValue('')}
+            onClick={() => {
+              setSearchValue('');
+              inputRef.current.focus()
+            }}
             xmlns="http://www.w3.org/2000/svg"
             class={styles.delete}
+            
             fill="none"
             viewBox="0 0 24 24"
             stroke-width="1.5"
